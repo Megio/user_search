990b1a1532f7e9412de54618fbdcce5ed129e344
Mattia Magi <mattia.magi91@gmail.com>
2022-07-13 02:58:02 +0200

    wip: jest

---
+++ src/utils/nullable/index.spec.ts
@@ -0,0 +1,16 @@
+import * as t from "io-ts";
+import * as E from "fp-ts/Either";
+import nullable from "./index";
+
+describe("nullable", () => {
+    it("should create a decoder which can handle the specified type and null", () => {
+        const FooCodec = t.type({
+            foo: t.string,
+        });
+
+        const NullableFooCodec = nullable(FooCodec);
+
+        expect(E.isRight(NullableFooCodec.decode({ foo: "hello" }))).toBe(true);
+        expect(E.isRight(NullableFooCodec.decode(null))).toBe(true);
+    });
+});
